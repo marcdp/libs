0205ebd3460ba34ce34964ad619af83919104786
Marc Delos Poch <marcdp@dprojects.com>
2026-09-07 12:03:54 +0200

    removed obsolete project DProjects.Queues.SqlServer

--- src/DProjects.Queues.SqlServer/Assembly.cs
+++
@@ -1,9 +0,0 @@
-using DProjects.Factories;
-
-namespace DProjects.Queues.SqlServer {
-
-    public class Assembly : IAssembly {
-        public static System.Reflection.Assembly Instance = typeof(Assembly).Assembly;
-    }
-
-}
