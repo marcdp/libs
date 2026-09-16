bd3e36849728600802f84893e0b6f040e319ca73
Marc Delos Poch <marcdp@dprojects.com>
2026-09-16 18:44:30 +0200

    Changed project type DProjects.XShell

---
+++ src/DProjects.XShell/Configuration.cs
@@ -0,0 +1,20 @@
+using Microsoft.JSInterop;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
+namespace DProjects.XShell;
+
+public static class X3Web {
+
+    // methods
+    public static void AddXShell(IServiceCollection services) {
+        // register web-specific services
+    }
+
+    public static void MapXShell(WebApplication app) {
+        // middleware
+        // endpoints
+        // static files
+    }
+
+}
