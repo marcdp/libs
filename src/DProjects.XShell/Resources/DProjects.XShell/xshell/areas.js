527f381c10dec897b7359f237ddc33eb1894767c
Marc Delos Poch <marcdp@dprojects.com>
2026-09-20 18:18:14 +0200

    DPRojects.XShell area.js rewrited

--- src/DProjects.XShell/Resources/DProjects.XShell/xshell/areas.js
+++ src/DProjects.XShell/Resources/DProjects.XShell/xshell/areas.js
@@ -1,71 +1,135 @@
-
 // default
 export default class Areas {
-    
-    
-    // vars
-    _currentAreaName = null;
-    _areas = [];
 
+    // vars
+    _bus = null;
+    _areas = [];
+    _currentAreaId = null;
 
     // ctor
     constructor({ config, bus }) {
-        // listen to navigation end to track current area
-        bus.addEventListener("xshell:navigation:end", (evt) => {
-            const src = evt.detail.src;
-            const areaId = this.resolveAreaId(src);
-            if (this._currentAreaName != areaId) {
-                this._currentAreaName = areaId;
-                bus.emit("xshell:area:change", { areaId: areaId } );
-            }
-        });
+        this._bus = bus;
+
+        const areasConfig = config.xshell.areas || {};
+        const definitions = areasConfig.definitions || {};
+        const defaultAreaId = areasConfig.default || null;
+
         // create areas
-        const areasConfig = config.xshell.areas;
-        for (const [areaId, areaConfig] of Object.entries(areasConfig.definitions)) {
-            const area = {
+        const areas = [];
+
+        for (const [areaId, areaConfig] of Object.entries(definitions)) {
+            const prefix = this._normalizePrefix(areaConfig.prefix);
+
+            areas.push(Object.freeze({
+                ...areaConfig,
                 id: areaId,
                 label: areaConfig.label || areaId,
                 icon: areaConfig.icon || null,
-                prefix: areaConfig.prefix || "",
+                prefix,
                 home: areaConfig.home || null,
-                modules: areaConfig.modules || [],
+                modules: Object.freeze([...(areaConfig.modules || [])]),
                 order: areaConfig.order || 0,
-                default: areasConfig.default === areaId
-            };
-            this._areas.push(Object.freeze(area));
+                default: areaId === defaultAreaId
+            }));
         }
-        // sort areas by default, order, label
-        this._areas.sort( (a,b) => {
-            if (a.default != b.default) return a.default ? -1 : 1;
-            if (a.order == b.order) return a.label.localeCompare(b.label);
-            return (a.order - b.order);
+
+        // validate
+        this._validateAreas(areas, defaultAreaId);
+
+        // sort for presentation
+        areas.sort((a, b) => {
+            if (a.default !== b.default) return a.default ? -1 : 1;
+            if (a.order !== b.order) return a.order - b.order;
+            return a.label.localeCompare(b.label);
         });
-        // freeze areas
-        this._areas = Object.freeze(this._areas);
+
+        this._areas = Object.freeze(areas);
+
+        // current area initially defaults to configured default
+        this._currentAreaId = this.getDefaultArea()?.id || null;
+
+        // navigation determines current area from URL prefix
+        bus.addEventListener("xshell:navigation:end", evt => {
+            const areaId = this.resolveAreaId(evt.detail.src);
+            if (areaId) this._setCurrentArea(areaId);
+        });
     }
 
-    // get areas
+    // areas
     getAreas() {
         return this._areas;
     }
+
+    getArea(id) {
+        return this._areas.find(area => area.id === id) || null;
+    }
+
     getDefaultArea() {
-        return this._areas.find( area => area.default ) || this._areas[0];
+        return this._areas.find(area => area.default) || this._areas[0] || null;
     }
+
     getCurrentArea() {
-        return this._areas.find(area => area.id === this._currentAreaName) || this.getDefaultArea();
+        return this.getArea(this._currentAreaId) || this.getDefaultArea();
     }
-    resolveAreaId(src) {
-        // get area id by src
-        // search by prefixes
-        for (const area of this._areas) {
-            for(const prefix of [area.prefix]) {
-                if (src.startsWith(prefix)) {
-                    return area.id;
-                }
+
+    // resolve area from navigation URL
+    resolveAreaId(href) {
+        if (!href) return null;
+
+        // longest prefix first, so /admin/tools wins over /admin
+        const areas = [...this._areas]
+            .sort((a, b) => b.prefix.length - a.prefix.length);
+
+        for (const area of areas) {
+            if (this._matchesPrefix(href, area.prefix)) {
+                return area.id;
             }
         }
+
         return null;
     }
 
-}
+    // private
+    _setCurrentArea(areaId) {
+        if (this._currentAreaId === areaId) return;
 
+        const area = this.getArea(areaId);
+        if (!area) throw new Error(`Unknown area '${areaId}'`);
+
+        this._currentAreaId = areaId;
+        this._bus.emit("xshell:area:change", { areaId });
+    }
+
+    _normalizePrefix(prefix) {
+        prefix = prefix || "/";
+
+        if (!prefix.startsWith("/")) prefix = "/" + prefix;
+        if (prefix.length > 1 && prefix.endsWith("/")) prefix = prefix.slice(0, -1);
+
+        return prefix;
+    }
+
+    _matchesPrefix(href, prefix) {
+        if (prefix === "/") return href.startsWith("/");
+
+        return href === prefix ||
+               href.startsWith(prefix + "/") ||
+               href.startsWith(prefix + "?");
+    }
+
+    _validateAreas(areas, defaultAreaId) {
+        if (defaultAreaId && !areas.some(area => area.id === defaultAreaId)) {
+            throw new Error(`Default area '${defaultAreaId}' is not defined`);
+        }
+
+        const prefixes = new Set();
+
+        for (const area of areas) {
+            if (prefixes.has(area.prefix)) {
+                throw new Error(`Duplicate area prefix '${area.prefix}'`);
+            }
+
+            prefixes.add(area.prefix);
+        }
+    }
+}
